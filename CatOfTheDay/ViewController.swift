830e51121ec33df573a7383e7a31a0f9406840b6
Artem LAPOV <artemlapov@me.com>
2022-11-05 17:27:26 +0100

    Initial Commit

---
+++ CatOfTheDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatOfTheDay
+//
+//  Created by Artem Lapov on 05.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
